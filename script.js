6702ce32475c41328d9998fe9c6bc88c0a246e7a
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 15:48:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+// function firstChar(text) {
+//   // your code here
+// }
+
+// // Do not change the code below
+
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
+
+
 function firstChar(text) {
-  // your code here
+  // Remove leading and trailing whitespaces
+  const trimmedText = text.trim();
+
+  // Return the first character or an empty string if the string is empty
+  return trimmedText.length > 0 ? trimmedText[0] : '';
 }
 
-// Do not change the code below
-
+// Example
 const text = prompt("Enter text:");
 alert(firstChar(text));
